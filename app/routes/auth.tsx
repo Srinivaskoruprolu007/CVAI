d47045a1201fa2ed35f5c22a3e0934ee3bf2e784
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-07-21 15:24:15 +0530

    authentication is done

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,3 +1,7 @@
+import {usePuterStore} from "~/lib/puter";
+import {useEffect} from "react";
+import {useLocation, useNavigate} from "react-router";
+
 export const meta = () => ([
     {
         title: "CVAI | Auth",
@@ -9,11 +13,50 @@ export const meta = () => ([
 ])
 
 const Auth = () => {
+    const {isLoading, auth} = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split('next=')[1]
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (auth.isAuthenticated) {
+            navigate(next)
+        }
+    }, [auth.isAuthenticated]);
     return (
         <main className={"bg-[url('/images/auth-bg.svg')] bg-cover min-h-screen flex items justify-center"}>
             <div className={"gradient-border shadow-lg"}>
                 <section className={"flex flex-col gap-8 bg-white rounded-2xl p-10"}>
-                    
+                    <div className={"flex flex-col items-center gap-2 text-center"}>
+                        <h1>
+                            Welcome to CVAI!
+                        </h1>
+                        <h2>
+                            Log in to Continue Your Job Journey
+                        </h2>
+                    </div>
+                    <div>
+                        {isLoading ? (
+                            <button className={"auth-button animate-pulse"}>
+                                <p>Signing in...</p>
+                            </button>
+                        ) : (
+                            <>
+                                {auth.isAuthenticated ? (
+                                    <button className={"auth-button"} onClick={auth.signOut}>
+                                        <p>
+                                            Log out
+                                        </p>
+                                    </button>
+                                ) : (
+                                    <button className={"auth-button"} onClick={auth.signIn}>
+                                        <p>
+                                            Log in
+                                        </p>
+                                    </button>
+                                )}
+                            </>
+                        )}
+                    </div>
                 </section>
             </div>
         </main>
