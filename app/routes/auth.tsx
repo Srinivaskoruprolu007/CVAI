c8bf99213cb3ff66a12bd0dcb806fb28e91e999a
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-07-21 14:54:01 +0530

    feat : Home page is build

---
+++ app/routes/auth.tsx
@@ -0,0 +1,22 @@
+export const meta = () => ([
+    {
+        title: "CVAI | Auth",
+
+    },
+    {
+        name: "description", content: "Log into your account"
+    }
+])
+
+const Auth = () => {
+    return (
+        <main className={"bg-[url('/images/auth-bg.svg')] bg-cover min-h-screen flex items justify-center"}>
+            <div className={"gradient-border shadow-lg"}>
+                <section className={"flex flex-col gap-8 bg-white rounded-2xl p-10"}>
+                    
+                </section>
+            </div>
+        </main>
+    )
+}
+export default Auth
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,39 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type {Route} from "./+types/home";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    return [
+        {title: 'CVAI'},
+        {name: "description", content: "Your Curriculum Vitae meets AI"},
+    ];
 }
 
 export default function Home() {
-  return <Welcome />;
+    return <main className={"bg-[url('/images/bg-main.svg')] bg-cover"}>
+        <Navbar/>
+        <section className={"main-section"}>
+            <div className={"page-heading"}>
+                <h1>
+                    Track your applications and Resume Ratings
+                </h1>
+                <h2>
+                    Review you submissions and check AI-powered feedback.
+                </h2>
+
+            </div>
+            {resumes.length > 0 && (
+
+                <div className={"resumes-section"}>
+
+                    {resumes.map((resume) => {
+                        return (
+                            <ResumeCard key={resume.id} resume={resume}/>
+                        )
+                    })}
+                </div>
+            )}
+        </section>
+    </main>;
 }
